ff1ffd734e9c659caf2d3131b6fbc8db7b92a104
Gokulpriyan27 <107754768+Gokulpriyan27@users.noreply.github.com>
2023-06-02 20:59:03 +0530

    changes

--- Utils/passport.js
+++ Utils/passport.js
@@ -98,15 +98,23 @@ const User = require('../models/userModel');
   );
   
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (error) {
-    done(error);
-  }
-});
+// passport.deserializeUser(async (id, done) => {
+//   try {
+//     const user = await User.findById(id);
+//     done(null, user);
+//   } catch (error) {
+//     done(error);
+//   }
+// });
+
+passport.serializeUser((user,done)=>{
+  done(null,user)
+})
+
+passport.deserializeUser((user,done)=>{
+  done(null,user)
+})
